1f4cc9f65d100fb5974bb0c5ff33e220c12b5cbe
Sarmurlocu <svvys543@gmail.com>
2026-03-05 10:26:32 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // ⚠️ 請將這裡替換成你 Firebase 後台提供的真實設定值！
 const firebaseConfig = {
-  apiKey: "AIzaSyYourAPIKeyHere...",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdefg..."
+  apiKey: "AIzaSyACsOsE467bbOGrHdkxjUhcdPL-i_F_rNM",
+  authDomain: "sizzle-drizzle-restaurant.firebaseapp.com",
+  projectId: "sizzle-drizzle-restaurant",
+  storageBucket: "sizzle-drizzle-restaurant.firebasestorage.app",
+  messagingSenderId: "713438474887",
+  appId: "1:713438474887:web:f4a1514105ad9beac7be22"
 };
 
 // 初始化 Firebase
